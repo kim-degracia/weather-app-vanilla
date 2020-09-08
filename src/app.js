b4b2fe69a1923e5298096b5d62746147fa087449
kim-degracia <69057610+kim-degracia@users.noreply.github.com>
2020-09-07 23:52:13 -0400

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -50,7 +50,18 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "12f8e457ba0d8b6c71f0515f7f8b361b";
-let city = "Paris";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "12f8e457ba0d8b6c71f0515f7f8b361b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
