4a5de91a20373cb5b2ba0ec37962d250e670d622
kim-degracia <69057610+kim-degracia@users.noreply.github.com>
2020-09-13 17:19:22 -0400

    Weather at different times

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ function formatDate(timestamp) {
     "Saturday",
   ];
   let day = days[date.getDay()];
-  return `${day} ${hours}:${minutes}`;
+  return `${day} ${formatHours(timestamp)}`;
 }
 
 function displayTemperature(response) {
@@ -51,10 +51,50 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes}`;
+}
+
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+  for (let index = 0; index < 6; index++) {
+    let forecast = response.data.list[index];
+    forecastElement.innerHTML += `  
+        <div class="col-2">
+          <h3>
+            ${formatHours(forecast.dt * 1000)}
+          </h3>
+          <img src="http://openweathermap.org/img/wn/${
+            forecast.weather[0].icon
+          }@2x.png"
+          />
+          <div class="weather-forecast-temperature">
+            <strong>${Math.round(
+              forecast.main.temp_max
+            )}°</strong> ${Math.round(forecast.main.temp_min)}°
+          </div>  
+        </div>`;
+  }
+}
+
 function search(city) {
   let apiKey = "12f8e457ba0d8b6c71f0515f7f8b361b";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function handleSubmit(event) {
